dd28f2787f2e5ab2ec00d1e4b6e6fcdd6b269576
gustavomg21 <gmicho015@gmail.com>
2023-04-19 21:00:53 -0400

    Finished project

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,14 +1,56 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  // Prompt user for password criteria
+  var length = parseInt(prompt("Choose between 8 and 128 characthers"));
+
+  var lowercase = confirm("Do you want to include lowercase characters?");
+  var uppercase = confirm("Do you want to include uppercase characters?");
+  var number = confirm("Do you want to include numeric characters?");
+  var special = confirm("Do you want to include special characters?");
+
+  // Define character sets to include in password
+  var lowerList = "abcdefghijklmnopqrstuvwxyz";
+  var upperList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numberList = "0123456789";
+  var specialList = "!@#$%^&*()[]{}|\/?";
+
+  var requested = "";
+  if (lowercase) {
+    requested += lowerList;
+  }
+  if (uppercase) {
+    requested += upperList;
+  }
+  if (number) {
+    requested += numberList;
+  }
+  if (special) {
+    requested += specialList;
+  }
+  console.log(requested);
+  
+  // Generate password
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * requested.length);
+    password += requested[randomIndex];
+  }
+  
+  return password;
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  confirm("This is your password: \n"+ password)
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # PasswordGen
 This is a password generator called PaddwordGen. You can customize the length, complexity, and character types of your password, tailoring it to your specific security needs. The generated passwords are random, making them highly secure.
+
+## Description
+
+- I was motivated by my passion in computer science to finish this school project
+- This project is a webpage that allows you to cuztomise a password.
+- It gives you a new random password after submiting your requirements
+
+## Table of Contents 
+
+Link to the contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+N/A
+
+## Usage
+
+To use this website open it on a webroser and scroll over the different sections and links
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to the LICENSE in the repo.
